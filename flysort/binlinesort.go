eed8d2c2f9db737ccf56b00ac67328aa726acbc9
Roman Covanyan <covrom@gmail.com>
2019-02-22 18:00:29 +0300

    test

--- flysort/binlinesort.go
+++ flysort/binlinesort.go
@@ -39,37 +39,35 @@ func LineUnrollInsert(a heapInts, num int) heapInts {
 	if len(a) < heapLimit {
 		a = append(a, num)
 	}
-	for i := 0; i < len(a); i++ {
+	ln4 := len(a) &^ 3
+	for i := 0; i < ln4; i += 4 {
 		v := a[i]
-		if i+3 < len(a) {
-			aa := a[i+1 : i+4]
-			_ = aa[2]
-			b0, b1, b2, b3 := v >= num, aa[0] >= num, aa[1] >= num, aa[2] >= num
-			switch {
-			case b0:
-				copy(a[i+1:], a[i:])
-				a[i] = num
-				return a
-			case b1:
-				copy(a[i+2:], a[i+1:])
-				a[i+1] = num
-				return a
-			case b2:
-				copy(a[i+3:], a[i+2:])
-				a[i+2] = num
-				return a
-			case b3:
-				copy(a[i+4:], a[i+3:])
-				a[i+3] = num
-				return a
-			}
-			i += 3
-		} else {
-			if v >= num {
-				copy(a[i+1:], a[i:])
-				a[i] = num
-				return a
-			}
+		aa := a[i+1 : i+4]
+		_ = aa[2]
+		b0, b1, b2, b3 := v >= num, aa[0] >= num, aa[1] >= num, aa[2] >= num
+		var j int8
+		switch {
+		case b0:
+			j = 0
+		case b1:
+			j = 1
+		case b2:
+			j = 2
+		case b3:
+			j = 3
+		default:
+			continue
+		}
+		copy(a[j+1:], a[j:])
+		a[j] = num
+		return a
+	}
+	for i := ln4; i < len(a); i++ {
+		v := a[i]
+		if v >= num {
+			copy(a[i+1:], a[i:])
+			a[i] = num
+			return a
 		}
 	}
 	return a
