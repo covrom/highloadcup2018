1a452c305bff36fbc37c9757101e0b6b4ab92825
Roman TSovanyan <covrom@gmail.com>
2019-02-22 23:15:45 +0300

    bubble insert

--- flysort/binlinesort.go
+++ flysort/binlinesort.go
@@ -72,3 +72,112 @@ func LineUnrollInsert(a heapInts, num int) heapInts {
 	}
 	return a
 }
+
+func BubbleInsert(a heapInts, num int) heapInts {
+	if len(a) < heapLimit {
+		a = append(a, num)
+	} else {
+		if a[len(a)-1] < num {
+			return a
+		}
+		a[len(a)-1] = num
+	}
+	i := len(a) - 1
+	for i >= 0 {
+		f := -1
+		for j := i - 8; j < i; j++ {
+			if j >= 0 {
+				if a[j] > num {
+					f = j
+					break
+				}
+			}
+		}
+		if f >= 0 {
+			switch i - f {
+			case 1:
+				a[f+1] = a[f]
+			case 2:
+				v1 := a[f]
+				v2 := a[f+1]
+				a[f+1] = v1
+				a[f+2] = v2
+			case 3:
+				v1 := a[f]
+				v2 := a[f+1]
+				v3 := a[f+2]
+				a[f+1] = v1
+				a[f+2] = v2
+				a[f+3] = v3
+			case 4:
+				v1 := a[f]
+				v2 := a[f+1]
+				v3 := a[f+2]
+				v4 := a[f+3]
+				a[f+1] = v1
+				a[f+2] = v2
+				a[f+3] = v3
+				a[f+4] = v4
+			case 5:
+				v1 := a[f]
+				v2 := a[f+1]
+				v3 := a[f+2]
+				v4 := a[f+3]
+				v5 := a[f+4]
+				a[f+1] = v1
+				a[f+2] = v2
+				a[f+3] = v3
+				a[f+4] = v4
+				a[f+5] = v5
+			case 6:
+				v1 := a[f]
+				v2 := a[f+1]
+				v3 := a[f+2]
+				v4 := a[f+3]
+				v5 := a[f+4]
+				v6 := a[f+5]
+				a[f+1] = v1
+				a[f+2] = v2
+				a[f+3] = v3
+				a[f+4] = v4
+				a[f+5] = v5
+				a[f+6] = v6
+			case 7:
+				v1 := a[f]
+				v2 := a[f+1]
+				v3 := a[f+2]
+				v4 := a[f+3]
+				v5 := a[f+4]
+				v6 := a[f+5]
+				v7 := a[f+6]
+				a[f+1] = v1
+				a[f+2] = v2
+				a[f+3] = v3
+				a[f+4] = v4
+				a[f+5] = v5
+				a[f+6] = v6
+				a[f+7] = v7
+			case 8:
+				v1 := a[f]
+				v2 := a[f+1]
+				v3 := a[f+2]
+				v4 := a[f+3]
+				v5 := a[f+4]
+				v6 := a[f+5]
+				v7 := a[f+6]
+				v8 := a[f+7]
+				a[f+1] = v1
+				a[f+2] = v2
+				a[f+3] = v3
+				a[f+4] = v4
+				a[f+5] = v5
+				a[f+6] = v6
+				a[f+7] = v7
+				a[f+8] = v8
+			}
+			a[f] = num
+		}
+		i -= 8
+	}
+	return a
+}
--- flysort/flysort_test.go
+++ flysort/flysort_test.go
@@ -65,6 +65,22 @@ func BenchLineUnrollInsert(b *testing.B) {
 	}
 }
 
+func BenchBubbleInsert(b *testing.B) {
+	if Ints == nil {
+		fillInts()
+	}
+	elt := 0
+	h := make(heapInts, 0, heapLimit)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		h = BubbleInsert(h, Ints[elt])
+		elt++
+		if elt >= len(Ints) {
+			elt = 0
+		}
+	}
+}
+
 func BenchmarkInserts(b *testing.B) {
 	if Ints == nil {
 		fillInts()
@@ -73,21 +89,25 @@ func BenchmarkInserts(b *testing.B) {
 	b.Run("SortInsert100", BenchSortInsert)
 	b.Run("LineInsert100", BenchLineInsert)
 	b.Run("LineUnrollInsert100", BenchLineUnrollInsert)
+	b.Run("BubbleInsert100", BenchBubbleInsert)
 
 	heapLimit = 1000
 	b.Run("SortInsert1000", BenchSortInsert)
 	b.Run("LineInsert1000", BenchLineInsert)
 	b.Run("LineUnrollInsert1000", BenchLineUnrollInsert)
+	b.Run("BubbleInsert1000", BenchBubbleInsert)
 
 	heapLimit = 10000
 	b.Run("SortInsert10000", BenchSortInsert)
 	b.Run("LineInsert10000", BenchLineInsert)
 	b.Run("LineUnrollInsert10000", BenchLineUnrollInsert)
+	b.Run("BubbleInsert10000", BenchBubbleInsert)
 
 	heapLimit = 100000
 	b.Run("SortInsert100000", BenchSortInsert)
 	b.Run("LineInsert100000", BenchLineInsert)
 	b.Run("LineUnrollInsert100000", BenchLineUnrollInsert)
+	b.Run("BubbleInsert100000", BenchBubbleInsert)
 }
 
 func TestAll(t *testing.T) {
@@ -98,11 +118,12 @@ func TestAll(t *testing.T) {
 		fillInts()
 	}
 
-	heapLimit = 10
+	heapLimit = 50
 
 	h1 := make(heapInts, 0, heapLimit)
 	h2 := make(heapInts, 0, heapLimit)
 	h3 := make(heapInts, 0, heapLimit)
+	h4 := make(heapInts, 0, heapLimit)
 
 	elt := 0
 	for i := 0; i < 100; i++ {
@@ -110,6 +131,7 @@ func TestAll(t *testing.T) {
 		h1 = SortInsert(h1, v)
 		h2 = LineInsert(h2, v)
 		h3 = LineUnrollInsert(h3, v)
+		h4 = BubbleInsert(h4, v)
 		elt++
 		if elt >= len(Ints) {
 			elt = 0
@@ -122,13 +144,16 @@ func TestAll(t *testing.T) {
 	t.Log(h2)
 	t.Log(len(h3))
 	t.Log(h3)
+	t.Log(len(h4))
+	t.Log(h4)
 
 	for i := range h2 {
 		v1 := h1[i]
 		v2 := h2[i]
 		v3 := h3[i]
-		if v1 != v2 || v2 != v3 {
-			t.Errorf("idx=%v, v1=%v, v2=%v, v3=%v", i, v1, v2, v3)
+		v4 := h4[i]
+		if v1 != v2 || v2 != v3 || v3 != v4 {
+			t.Errorf("idx=%v, v1=%v, v2=%v, v3=%v, v4=%v", i, v1, v2, v3, v4)
 		}
 	}
 }
