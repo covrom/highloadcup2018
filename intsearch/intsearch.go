b11caab6a359205e8ebb6bb91c71e9b055e390b2
Roman TSovanyan <covrom@gmail.com>
2019-02-23 13:40:39 +0300

    unsafe

--- intsearch/intsearch.go
+++ intsearch/intsearch.go
@@ -165,12 +165,12 @@ func csbinSearch(a []uint32, x uint32) uint32 {
 }
 
 func unsafeBinSearch(a []uint32, x uint32) uint32 {
+	p := (*[1 << 28]uint32)(unsafe.Pointer(&a[0]))
 	n := uint32(len(a))
 	i, j := uint32(0), n
-	// p := (*[1 << 33]uint32)(unsafe.Pointer(&a[0]))
 	for i < j {
 		h := (i + j) >> 1
-		if (*[1 << 33]uint32)(unsafe.Pointer(*(*uintptr)(unsafe.Pointer(&a))))[h] < x { // a[h] < x {
+		if p[h] < x {
 			i = h + 1
 		} else {
 			j = h
